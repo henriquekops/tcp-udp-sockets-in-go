290a707bda0f1856d4cc6e5a9e3e84c900e49ccd
henriquekops <henriquekops@gmail.com>
2020-04-10 16:10:32 -0300

    fix file transport for TCP

--- sockets/TCP/tcpClient.go
+++ sockets/TCP/tcpClient.go
@@ -2,13 +2,11 @@ package TCP
 
 import (
 	c "../common"
-	"bufio"
 	"fmt"
 	"io"
 	"net"
 	"os"
 	"strconv"
-	"strings"
 )
 
 const clientType = "TCP:Client"
@@ -23,55 +21,18 @@ func CreateTCPClient(network string, serverAddress string) {
 	defer connection.Close()
 
 	c.Log(clientType, "Connected to '" + connection.RemoteAddr().String() + "'")
-	c.Log(clientType, "Input your data to send to server\n['exit' to quit]")
+	//c.Log(clientType, "Input your data to send to server\n['exit' to quit]")
 
-	for {
-		// Args
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("$ ")
-		input, _ := reader.ReadString('\n')
-		parsedInput := strings.TrimSpace(input)
-
-		// Exit?
-		if parsedInput == "exit" {
-			_, err = fmt.Fprintf(connection, parsedInput + "\n")
-			c.CheckError(clientType, err)
-			c.Log(clientType, "Safe quit... Bye :)")
-			return
-		}
-
-		// Send file to TCP server
-		fileName, fileSize, err := sendFileToServer(connection, parsedInput)
-		if err == nil {
-			c.Log(clientType, "SENT " + fileName + ", " + strconv.FormatInt(fileSize, 10) + " BYTES")
-
-			// Wait for response
-			serverMsg, _ := bufio.NewReader(connection).ReadString('\n')
-			parsedMsg := strings.TrimSuffix(strings.TrimSpace(serverMsg),  "\n")
-			c.Log(clientType, "RECEIVED -> '" + parsedMsg + "'")
-		}
-	}
-}
-
-func sendFileToServer(connection net.Conn, filename string) (string, int64, error) {
-	// Open file
-	buffer := make([]byte, c.BUFFER_SIZE)
-
-	file, err := os.Open(filename)
+	// Send file to TCP server
+	file, err := os.Open("./test/send/test1.txt")
 	if err != nil {
 		c.Log(clientType, "File does not exist!")
-		return "", 0, err
+		return
 	}
 	defer file.Close()
 
-	// Send file
 	fileInfo, _ := file.Stat()
-
-	_, err = connection.Write(buffer)
-	c.CheckError(clientType, err)
-
 	_, err =  io.Copy(connection, file)
 	c.CheckError(clientType, err)
-
-	return fileInfo.Name(), fileInfo.Size(), nil
+	c.Log(clientType, "SENT " + strconv.FormatInt(fileInfo.Size(), 10) + " BYTES")
 }
--- sockets/TCP/tcpServer.go
+++ sockets/TCP/tcpServer.go
@@ -7,7 +7,6 @@ import (
 	"net"
 	"os"
 	"strconv"
-	"strings"
 )
 
 const serverType  = "TCP:Server"
@@ -35,44 +34,21 @@ func CreateTCPServer(network string, serverPort string) {
 
 func handleClient(connection net.Conn) {
 	// New connection
-	buffer := make([]byte, c.BUFFER_SIZE)
 	remoteAddr := connection.RemoteAddr().String()
 	c.Log(serverType, "NEW CONNECTION (SOURCE=''" + remoteAddr + "')")
 	defer connection.Close()
 
-	for {
-		// Capture message
-		length, err := connection.Read(buffer)
-		c.CheckError(serverType, err)
-		clientMsg := strings.TrimSuffix(strings.TrimSpace(string(buffer[:length])), "\n")
-
-		// Aborted ?
-		if clientMsg == "exit" {
-			c.Log(serverType, "Connection " + remoteAddr + " aborted")
-			break
-		}
-
-		// File received
-		fileName, fileSize := receiveFile(clientMsg, connection)
-		c.Log(serverType, "RECEIVED (SOURCE='"+ remoteAddr +"') -> '" + fileName + ", " + strconv.FormatInt(fileSize, 10) + " BYTES")
-
-		// Return to client
-		c.Log(serverType, "SENDING (SOURCE='" + remoteAddr + "') OK")
-		_, err = connection.Write([]byte("OK\n"))
-		c.CheckError(serverType, err)
-	}
-}
-
-func receiveFile(filename string, conn net.Conn) (string, int64){
 	// Create new file
-	file, err := os.Create(filename)
+	file, err := os.Create("./test/receive/TCP_RECEIVED.txt")
 	c.CheckError(serverType, err)
 	defer file.Close()
 
 	// Copy file data
-	_, err = io.Copy(conn, file)
+	_, err = io.Copy(file, connection)
 	c.CheckError(serverType, err)
-	fileInfo, _ := file.Stat()
 
-	return fileInfo.Name(), fileInfo.Size()
+	// Log
+	fileInfo, _ := file.Stat()
+	c.Log(serverType, "RECEIVED " + strconv.FormatInt(fileInfo.Size(), 10) + " BYTES")
+	c.Log(serverType, "CLOSING CONNECTION WITH " + remoteAddr)
 }
